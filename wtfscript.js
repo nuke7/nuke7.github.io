e182999c1b31c0ebc383146bb41b6b3aa78ebb63
nuke7 <gombos.marton@icloud.com>
2020-05-16 12:44:52 +0200

    fixed background and dialogs

--- wtfscript.js
+++ wtfscript.js
@@ -28,3 +28,54 @@ $('.card').click(function() {
 });
 
 console.log("Marton Gombos");
+
+$(function() {
+    $("#dialog-faith").dialog({
+        autoOpen: false,
+        show: {
+            effect: "bounce",
+            duration: 1000
+        },
+        hide: {
+            effect: "drop",
+            duration: 1000
+        },
+    });
+    $('#faith').on("click", function() {
+        $("#dialog-faith").dialog("open");
+    });
+});
+
+$(function() {
+    $("#dialog-rule").dialog({
+        autoOpen: false,
+        show: {
+            effect: "clip",
+            duration: 1000
+        },
+        hide: {
+            effect: "slide",
+            duration: 1000
+        },
+    });
+    $('#rule').on("click", function() {
+        $("#dialog-rule").dialog("open");
+    });
+});
+
+$(function() {
+    $("#dialog-master").dialog({
+        autoOpen: false,
+        show: {
+            effect: "drop",
+            duration: 1000
+        },
+        hide: {
+            effect: "clip",
+            duration: 1000
+        },
+    });
+    $('#master').on("click", function() {
+        $("#dialog-master").dialog("open");
+    });
+});
